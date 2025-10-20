558bcd4bb0069218b199d7e857077e0ec8e8aa58
Vlad Rublenko <vladik.rar@ukr.net>
2025-10-20 20:47:27 +0300

    initial commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String HYPHEN = "-";
+    private static final String UKR_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD = 10;
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equalsIgnoreCase(UKR_NATIONALITY)
+                && getPeriodInUkr(candidate.getPeriodsInUkr()) >= MIN_PERIOD;
+    }
+
+    private int getPeriodInUkr(String stringPeriod) {
+        String[] arrPeriod = stringPeriod.split(HYPHEN);
+        int from = Integer.parseInt(arrPeriod[0]);
+        int to = Integer.parseInt(arrPeriod[1]);
+
+        return to - from;
+    }
 }
